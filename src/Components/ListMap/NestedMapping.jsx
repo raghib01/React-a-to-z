122a21c9371d7944b03424e8233a4cb15cfb5477
rakib <rakibulislamragib2@gmail.com>
2024-09-22 01:26:02 +0600

    Nested Maooing Proccess

--- src/Components/ListMap/NestedMapping.jsx
+++ src/Components/ListMap/NestedMapping.jsx
@@ -51,7 +51,7 @@ export default function NestedMapping() {
   return (
     
     <div>
-       <h1 className='text-3xl font-bold text-black m-5 text-center'>Nested mapping in list data items</h1>
+       <h1 className='text-3xl font-bold text-black m-3 text-center'>Nested mapping in list data items</h1>
         {Users.map((User)=> 
             <div className='w-1/2 flex flex-col justify-center'>
                 <div className='bg-neutral-600 text-white m-2 text-xl'  key={User.id} >
@@ -59,7 +59,6 @@ export default function NestedMapping() {
                     <p>Age: {User.age}</p>
                     <p>Email: {User.email}</p>
                     <p> address : {User.address.city}</p>
-
                     {
                         User.phone.map((userPhone, number)=> 
                             

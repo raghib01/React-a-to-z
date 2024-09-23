e80b567b8bea43c95759b97d3e4498ef83cade7f
rakib <rakibulislamragib2@gmail.com>
2024-09-23 23:49:49 +0600

    basic hooks using functional method

---
+++ src/Components/Hooks_Usestate/Hooks.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export default function Hooks() {
+
+    // useState Hook
+  const [count, setCount] = useState(0);
+
+ const handleInc = () => {
+    setCount(count + 1)
+  }
+ const handleDec = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <div className=''>
+    <div className='text-center bg-black text-white'>
+        <h1 >Using Hooks in React</h1>
+         <p>React Hooks are a new feature in React 16.8.0 that allows you to use state and other React features without writing a class.</p>
+        <p>Some common hooks include useState, useEffect, and useContext.</p>
+    </div>
+      
+
+      <h1 className='text-3xl mx-10 my-5'>Count : {count}</h1>
+      <button onClick={handleInc} className='px-4 py-2 bg-rose-700 text-white m-4 font-semibold rounded-sm'>Increment</button>
+      <button onClick={handleDec} className='px-4 py-2 bg-rose-700 text-white m-4 font-semibold rounded-sm'>Decrement</button>
+
+
+    </div>
+  )
+}
